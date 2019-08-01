3bae4a2e520aeb3c8c231203bbad50d6c42f3543
JorgeOchoaDev <jl91ob@protonmail.comm>
2019-07-31 21:35:35 -0500

    third exercise

--- Js2/Js/main.js
+++ Js2/Js/main.js
@@ -4,7 +4,10 @@ var radius = diameter/2;
 console.log(PI*(radius*radius));
 /*Dado un número{}verificar si es par*/
 var number = 0;
-if(number%2===0){
+if (number>=10){
+  console.log("your number is more than one digit!");
+}
+else if(number%2===0){
   console.log(number+" is even");
 }
 else{
