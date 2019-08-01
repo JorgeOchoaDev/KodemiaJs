0abf237fe6112aa68d913b86509cb87064d97236
JorgeOchoaDev <jl91ob@protonmail.comm>
2019-07-31 20:57:08 -0500

    second exercise

--- Js2/Js/main.js
+++ Js2/Js/main.js
@@ -2,3 +2,11 @@ const PI = 3.1416;
 var diameter = 10;
 var radius = diameter/2;
 console.log(PI*(radius*radius));
+/*Dado un número{}verificar si es par*/
+var number = 0;
+if(number%2===0){
+  console.log(number+" is even");
+}
+else{
+  console.log(number+" is odd");
+}
